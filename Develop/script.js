19b9c3d867ab95123a3cda9491eaefe3f7e9aa70
Hibo Ali <hiba.mumin22@gmail.com>
2022-01-11 12:40:27 -0600

    added uppercase and lowercase variables

--- Develop/script.js
+++ Develop/script.js
@@ -36,6 +36,64 @@ function generatePassword() {
 
     //  lowercase, uppercase, special characters
 
+    var lowerCase = [
+      'a',
+      'b',
+      'c',
+      'd',
+      'e',
+      'f',
+      'g',
+      'h',
+      'i',
+      'j',
+      'k',
+      'l',
+      'm',
+      'n',
+      'o',
+      'p',
+      'q',
+      'r',
+      's',
+      't',
+      'u',
+      'v',
+      'w',
+      'x',
+      'y',
+      'z',
+    ];
+
+    var upperCase = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z',
+    ];
+
 
   // #2 Validate the input
 
