1666a0680e3c178ae3ecf9fa121bbdf96eaf7acb
Hibo Ali <hiba.mumin22@gmail.com>
2022-01-11 12:52:11 -0600

    moved letters and numbers out of generatePassword function & created random integar function

--- Develop/script.js
+++ Develop/script.js
@@ -13,6 +13,105 @@ function writePassword() {
 
 }
 
+  //  function to create random integar
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  //  lowercase, uppercase, special characters
+
+  var lowerCase = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  var upperCase = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialCharacters = [
+      '`',
+      '~',
+      '!',
+      '@',
+      '#',
+      '$',
+      '%',
+      '^',
+      '&',
+      '*',
+      '(',
+      ')',
+      '-',
+      '_',
+      '+',
+      '=',
+      '[',
+      ']',
+      '{',
+      '}',
+      '|',
+      ';',
+      ':',
+      "'",
+      '.',
+      '<',
+      '>',
+      '?',
+      '/',
+
+  ];
+
 
 // define generate password by creating a function 
 function generatePassword() {
@@ -34,99 +133,7 @@ function generatePassword() {
     return "Please choose password length between 8 and 128 digits! \nPress the button to try again!";
   }
 
-    //  lowercase, uppercase, special characters
-
-    var lowerCase = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-      'x',
-      'y',
-      'z',
-    ];
-
-    var upperCase = [
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F',
-      'G',
-      'H',
-      'I',
-      'J',
-      'K',
-      'L',
-      'M',
-      'N',
-      'O',
-      'P',
-      'Q',
-      'R',
-      'S',
-      'T',
-      'U',
-      'V',
-      'W',
-      'X',
-      'Y',
-      'Z',
-    ];
-
-    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    var specialCharacters = [
-        '`',
-        '~',
-        '!',
-        '@',
-        '#',
-        '$',
-        '%',
-        '^',
-        '&',
-        '*',
-        '(',
-        ')',
-        '-',
-        '_',
-        '+',
-        '=',
-        '[',
-        ']',
-        '{',
-        '}',
-        '|',
-        ';',
-        ':',
-        "'",
-        '.',
-        '<',
-        '>',
-        '?',
-        '/',
-
-    ];
+  
 
 
   // #2 Validate the input
