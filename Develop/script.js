a1d78e0f54ce251fec38ac5a1066763f1b03a68c
Hibo Ali <hiba.mumin22@gmail.com>
2022-01-11 11:56:35 -0600

    outline for function

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,31 @@ function writePassword() {
 
 }
 
+
+// define generate password by creating a function 
+function generatePassword() {
+  console.log("Button is working")
+
+  // #1 prompt user for password criteria
+
+
+    //  password length 8 <128
+
+
+    //  lowercase, uppercase, special characters
+
+
+  // #2 Validate the input
+
+
+  //  #3 Generate password based on criteria
+
+
+  //  #4 Display password to the page
+    return "Generated password will go here!"
+}
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
